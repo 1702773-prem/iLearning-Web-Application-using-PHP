642a3bfbf98f6dd926a7c19d7133475caca092ab
Prem <ps09311@gmail.com>
2023-09-23 13:16:39 +0530

    Created Nav bar, Added background video and created banner section

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="Css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/a6998f0760.js" crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- Start Navigation bar -->
+
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top px-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">iLearning</a>
+    <span style="color:white; opacity:0.8">learn and implement</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse ms-5" id="navbarNavAltMarkup">
+      <ul class="navbar-nav">
+      <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Courses</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Contact us</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">About us</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+        
+      </ul>
+    </div>
+    
+  </div>
+</nav>
+
+<!-- End Navigation bar -->
+
+<!-- Start background video-section -->
+
+<div class="remove-vid-margin">
+  <div class="video-conatiner">
+     <video class="video" autoplay muted loop>
+      <source src="Videos/main_video.mp4">
+     </video>
+
+     <div class="video-overlay"></div>
+
+     <div class="video-content">
+      <h1 class="my-content">Welcome to iLearning</h1>
+      <small class="my-content">Leran and implement</small>
+      <button class="btn btn-danger">Get Started</button>
+     </div>
+  </div>
+</div>
+
+<!-- End background video-section -->
+
+<!-- Start banner -->
+
+<div class="container-fluid bg-danger banner">
+ <div class="txt-banner">
+ <i class="fa-solid fa-book-open"></i><span>100+ online courses</span>
+ </div>
+ <div class="txt-banner">
+ <i class="fa-solid fa-user-group"></i><span>Expert Instructor</span>
+ </div>
+ <div class="txt-banner">
+ <i class="fa-regular fa-keyboard"></i></i><span>Lifetime Access</span>
+ </div>
+ <div class="txt-banner">
+ <i class="fa-solid fa-indian-rupee-sign"></i><span>Money Back gaurantee*</span>
+ </div>
+</div>
+
+<!-- End banner -->
+
+
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
